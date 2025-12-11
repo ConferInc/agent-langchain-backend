8bde4c5974405b972b84995d1c0dc8b176c13a84
Harshit Kasaundhan <harshit@mail.confersolutions.ai>
2025-12-11 22:59:26 +0530

    Update lang.py

--- lang.py
+++ lang.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import os
 
+# Load env variables
 load_dotenv()
 
-gemini_api_key = os.getenv("GEMINI_API_KEY")
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
-llm = ChatGoogleGenerativeAI(
-    model="gemma-3-4b-it",
-    api_key=gemini_api_key
+# Initialize model
+llm = ChatOpenAI(
+    model="gpt-4.1-nano",
+    openai_api_key=openai_api_key,
+    openai_api_base="https://litellm.confer.today"
 )
 
 app = FastAPI()
@@ -22,3 +25,8 @@ class ChatRequest(BaseModel):
 def chat(req: ChatRequest):
     response = llm.invoke(req.message)
     return {"response": response.content}
+
+# Run on Uvicorn (PRODUCTION MODE)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("lang:app", host="0.0.0.0", port=4000)
